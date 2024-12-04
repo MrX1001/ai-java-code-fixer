abcb5f4ca6d5881d26156930553415a4fac63833
MrX1001 <40733245+MrX1001@users.noreply.github.com>
2024-12-04 15:03:50 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,10 @@
-null
+public class Main {   
+    //Check2
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("The result is: " + result);
+    }
+    public static int add(a, b) {
+        return a + b;
+    }
+}
