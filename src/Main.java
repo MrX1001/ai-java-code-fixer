a23057bcb8d7c39636da7a81dcd15c63e5e1b08a
MrX1001 <40733245+MrX1001@users.noreply.github.com>
2024-12-05 06:57:24 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {   
-    //Check4
+    //Check5
     public static void main(String[] args) {
         int result = add(5, 3);
         System.out.println("The result is: " + result);
